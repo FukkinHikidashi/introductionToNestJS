5ebf041c9b3660180e8e4ebb073599b52be57128
takano <s.t.fukkinghandson@gmail.com>
2022-10-13 20:21:57 +0900

    add test comment

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('how to migrate branch including PR')
 }
 bootstrap();
