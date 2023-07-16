b1f00b4b5898624bded8f5ca1bb024f19d9221e2
Takano Sho <42609724+FukkinHikidashi@users.noreply.github.com>
2023-07-16 09:01:42 +0900

    release to staging (#11)
    * add pika (#10)
    
    * Test merge option (#9)
    
    * add fuga
    
    * yarn install
    
    * add foo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('hoge')
-  console.log('fuga')
+  console.log('hoge');
+  console.log('fuga');
+  console.log('pika');
+  console.log('foo');
 }
 bootstrap();
