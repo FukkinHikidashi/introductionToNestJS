92e0ee3c25160ab024a64d7b155b0d0a98012942
Takano Sho <42609724+FukkinHikidashi@users.noreply.github.com>
2023-07-16 09:00:55 +0900

    Test merge option (#9)
    * add fuga
    
    * yarn install
    
    * add foo

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('hoge')
-  console.log('fuga')
-  console.log('pika')
+  console.log('hoge');
+  console.log('fuga');
+  console.log('pika');
+  console.log('foo');
 }
 bootstrap();
