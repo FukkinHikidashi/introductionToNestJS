92362cfcf159365f573a5e9c13502ff14779df58
Takano <s.t.fukkinghandson@gmail.com>
2023-08-21 10:05:17 +0900

    delete comments and add aaa

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('hoge');
-  console.log('fuga');
-  console.log('bin');
-  console.log('foo');
+  console.log('aaa');
 }
 bootstrap();
