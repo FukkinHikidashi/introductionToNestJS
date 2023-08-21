5611209e0c13b16bec0c6b9571e69ff765baa72d
Takano <s.t.fukkinghandson@gmail.com>
2023-08-21 17:04:23 +0900

    revert delete comments

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('aaa');
+  console.log('bbb');
+  console.log('ccc');
+  console.log('ddd');
+  console.log('eee');
+  console.log('fen');
 }
 bootstrap();
